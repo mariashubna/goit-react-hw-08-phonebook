5892edd1cb4d7d78e866fe9479052b1b9caff87e
mariashubna <127610359+mariashubna@users.noreply.github.com>
2023-12-24 15:49:06 +0000

    Deploying to gh-pages from @ mariashubna/goit-react-hw-08-phonebook@cadaa478a6e5e33f1f3bc6f005e3c9eecdaa7c6b 🚀

---
+++ static/js/680.302c05a5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"FriendNet Notebook"})})}}}]);
+//# sourceMappingURL=680.302c05a5.chunk.js.map
